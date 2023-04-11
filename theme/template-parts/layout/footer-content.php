474d3722b97569bdaf6831a9cb63094ed934f4ff
Nashmod <hello@obsidianlab.io>
2023-04-11 19:51:44 +0200

    chore: clean up

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -17,13 +17,13 @@ use ObsidianLab\Tailwind_Walker;
 
 		<div class="border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
 			<img id="site_logo" class="block h-12 w-auto" src="<?= esc_url(wp_get_attachment_image_src(get_theme_mod("custom_logo"), "full")[0] ?? "") ?>" alt="<?= esc_attr(get_bloginfo("name")) ?>">
-			<div class="footer__menu">
+			<div class="footer__menu pt-5 md:pt-0">
 				<?php wp_nav_menu([
 					"theme_location" => "menu-footer",
 					"menu_id" => "footer-menu",
 					"container" => "",
 					"items_wrap" => '%3$s',
-					"link_class" => "inline-flex uppercase items-center border-b-2 border-transparent px-5  pt-1 text-sm font-medium text-gray-500",
+					"link_class" => "inline-flex uppercase items-center border-b-2 border-transparent pr-5 md:px-5 pt-1 text-sm font-medium text-gray-500",
 					"walker" => new Tailwind_Walker(),
 				]); ?>
 			</div>
